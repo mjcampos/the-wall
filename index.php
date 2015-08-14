840ceef282c223510105f5d13260c71a3aadad22
Michael Campos <camposmj89@gmail.com>
2015-08-13 22:51:58 -0700

    second commit

--- index.php
+++ index.php
@@ -25,41 +25,78 @@
 
 				<div class="collapse navbar-collapse">
 					<ul class="nav navbar-nav navbar-right">
-						<li><p data-toggle="modal" data-target="#myModal">Log In</p></li>
+						<li><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Log In</button></li>
 					</ul>
 				</div>
 			</div>
 		</nav>
 
-		<div class="container">
+		<div class="container main">
 			<h1>The Wall</h1>
-			<h2>Welcome to The Wall, the hottest social networking site since Facebook.</h2>
-			<p>Since Facebook's collapse in 2019 from the rise of the infamous AI, The Bandito Machina, the online world has been looking for a savior for their selfies, trolls, and memes. The Wall is the savior they've been looking for.</p>
+			<h2>Welcome to The Wall, the hottest social networking site since Facebook's horrifying, yet very sensual, demise in 2019. Boy that had to have been one of the freakest years in human history.</h2>
+
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3">
+					<h2>Sign Up</h2>
+					<form>
+						<div class="form-group">
+							<label for="first_name">First Name</label>
+							<input type="text" class="form-control" id="first_name" placeholder="First Name">
+						</div>
+
+						<div class="form-group">
+							<label for="last_name">Last Name</label>
+							<input type="text" class="form-control" id="last_name" placeholder="Last Name">
+						</div>
+
+						<div class="form-group">
+							<label for="email">Email</label>
+							<input type="email" class="form-control" id="email" placeholder="Email">
+						</div>
+
+						<div class="form-group">
+							<label for="password">Password</label>
+							<input type="text" class="form-control" id="password" placeholder="Password">
+						</div>
+
+						<div class="form-group">
+							<label for="confirmation_password">Confirmation Password</label>
+							<input type="text" class="form-control" id="confirmation_password" placeholder="Confirmation Password">
+						</div>
+
+						<button type="submit" class="btn btn-primary">Sign Up</button>
+					</form>
+				</div>
+			</div>
 		</div>
 
-	<div class="container">
- 
-  <!-- Modal -->
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
-        </div>
-        <div class="modal-body">
-          <p>Some text in the modal.</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-      
-    </div>
-  </div>
-  
-</div>
+		<!-- Modal -->
+		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title" id="myModalLabel">Log In</h4>
+					</div>
+
+					<div class="modal-body">
+						<form>
+							<div class="form-group">
+								<label for="email">Email</label>
+								<input type="email" class="form-control" id="email" placeholder="Email">
+							</div>
+							<div class="form-group">
+								<label for="password">Password</label>
+								<input type="password" class="form-control" id="password" placeholder="Password">
+							</div>
+
+							<div class="modal-footer">
+								<button type="submit" class="btn btn-primary">Log In</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
